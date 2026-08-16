0932ad58e08c24a1776c6f9cc709e87663c392c6
Nguyễn Hà Tuấn Nguyên <nguyenhatuannguyen4104@gmail.com>
2026-08-16 22:10:16 +0700

    add force-dynamic to accent route

--- src/app/api/birthdays/accent/route.ts
+++ src/app/api/birthdays/accent/route.ts
@@ -1,1 +1,7 @@
-export { dynamic, POST } from '@/app/api/students/accent/route'
+import { POST as updateStudentAccent } from '@/app/api/students/accent/route'
+
+export const dynamic = 'force-dynamic'
+
+export async function POST(req: Request) {
+  return updateStudentAccent(req)
+}
